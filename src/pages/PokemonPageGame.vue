0ecd907a8704176f1aed52656d38a3c65a447b67
FranciscoMartinez-650 <paquito.martinez2502.pm@gmail.com>
2022-03-25 00:53:09 -0600

    Se comenzo juego de pokemon del curso de vue

---
+++ src/pages/PokemonPageGame.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>¿Quien es ese pokemon?</h1>
+    <PokemonPictures :pokemonId="40" :showPokemon="true"/>
+    <PokemonOption/>
+</template>
+
+<script>
+import  PokemonOption from '@/components/PokemonOption'
+import  PokemonPictures from '@/components/PokemonPictures'
+export default ({
+    components: {PokemonOption,PokemonPictures}
+})
+</script>
+<style>
+
+</style>
