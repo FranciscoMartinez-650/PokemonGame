2c5d8ba3eb1a7903a3d4c11ff8ce98f32d4cf9dd
FranciscoMartinez-650 <paquito.martinez2502.pm@gmail.com>
2022-03-28 19:14:41 -0600

    Finalización del video 92 del curso de VUE juego pokemon

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,3 +1,4 @@
+import pokemonAPI from "@/api/pokemonAPI"
 
 
 const getPokemons = () => {
@@ -5,9 +6,28 @@ const getPokemons = () => {
     return pokemonsArr.map((_, index)=> index+1)
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async() => {
     const mixedPokemons = getPokemons().sort(Math.random()-0.5)
-    console.log(mixedPokemons)
+    const pokemons = await getPokemonName(mixedPokemons.splice(0,4))
+    return pokemons
+}
+
+const getPokemonName = async({a,b,c,d}= []) => {
+    //const resp = await pokemonAPI.get(`/$1`)
+    //console.log(resp)
+    const promiseArr = [
+        pokemonAPI(`/${a}`),
+        pokemonAPI(`/${b}`),
+        pokemonAPI(`/${c}`),
+        pokemonAPI(`/${d}`),
+    ]
+    const [p1,p2,p3,p4] = await Promise.all(promiseArr)
+    return [
+        {name: p1.data.name, id: p1.data.id},
+        {name: p2.data.name, id: p2.data.id},
+        {name: p3.data.name, id: p3.data.id},
+        {name: p4.data.name, id: p4.data.id},
+    ]
 }
 
 export default getPokemonOptions;
